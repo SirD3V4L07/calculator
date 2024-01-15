a2212ef6988e7c175c904b9fb3bc7cb95bf27760
SirD3v4l07 <ascendantspiral@gmail.com>
2024-01-15 18:58:54 +0000

    Show inputs on display, clear display on clear key

--- script.js
+++ script.js
@@ -26,56 +26,82 @@ function divide (operand1, operand2) {
  */
 function button1() {
     input.push(1);
+    updateDisplay();
 }
 
 function button2() {
     input.push(2);
+    updateDisplay();
 }
 
 function button3() {
     input.push(3);
+    updateDisplay();
 }
 
 function button4() {
     input.push(4);
+    updateDisplay();
 }
 
 function button5() {
     input.push(5);
+    updateDisplay();
 }
 
 function button6() {
     input.push(6);
+    updateDisplay();
 }
 
 function button7() {
     input.push(7);
+    updateDisplay();
 }
 
 function button8() {
     input.push(8);
+    updateDisplay();
 }
 
 function button9() {
     input.push(9);
+    updateDisplay();
 }
 
 function button0() {
     input.push(0);
+    updateDisplay();
 }
 
 function buttonAdd() {
     input.push("+");
+    updateDisplay();
 }
 
 function buttonSubtract() {
     input.push("-");
+    updateDisplay();
 }
 
 function buttonMultiply() {
     input.push("*");
+    updateDisplay();
 }
 
 function buttonDivide() {
     input.push("/");
+    updateDisplay();
+}
+
+function buttonClear() {
+    input.length = 0;
+    updateDisplay();
+}
+
+
+function updateDisplay() {
+    // Show typed content on screen 
+    let calcDisplay = document.getElementById("display");
+    calcDisplay.innerText = input.join(" ");
 }
